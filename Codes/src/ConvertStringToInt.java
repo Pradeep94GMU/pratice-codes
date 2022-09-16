497c17efd0e4d89c6a7787a606c8f12075ee8568
Pradeep Ranjan <pradeep.ranjan7837@gmail.com>
2022-09-15 23:46:02 -0400

    fix: This program will convert any string into Integer

---
+++ Codes/src/ConvertStringToInt.java
@@ -0,0 +1,10 @@
+public class ConvertStringToInt {
+    public static void main(String[] args) {
+
+        String s = "123";
+        int x = Integer.parseInt(s);
+        System.out.println(x);
+
+
+    }
+}
